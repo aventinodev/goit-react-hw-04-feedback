4db8e7013f5062ad03822737e77c0547f011be85
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-03 21:58:49 +0200

    Create Section.styled.jsx

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  padding: 30px 60px;
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 32px;
+
+  font-size: 2.5rem;
+  font-weight: bold;
+  color: #212121;
+`;
