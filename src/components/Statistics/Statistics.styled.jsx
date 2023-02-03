09dd486888192d2efec7c212bdbd4dc6daf2d183
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-03 21:58:55 +0200

    Create Statistics.styled.jsx

---
+++ src/components/Statistics/Statistics.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  list-style: none;
+`;
+export const Stats = styled.li`
+  font-size: 1.5rem;
+
+  :not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
